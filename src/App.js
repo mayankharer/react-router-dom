84cec96ea9fcf84a20ee90dd45414287c387af95
mayankharer <mayankharer8@gmail.com>
2023-10-09 21:40:38 +0530

    Lesson 2 done with basics of routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>React-Router-Dom Tutorials</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        {/*Below Routes works like parent component for multiple routes*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
